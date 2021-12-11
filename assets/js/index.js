c1917cb5d15a1cfbdcb6cbab8ebc44688d510822
wildanptra <wildanptra03@gmail.com>
2021-12-11 15:31:59 +0700

    remove testimonial add gallery

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,34 +13,18 @@ window.onscroll = () =>  {
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:false,
-        margin:100,
+        margin:10,
         nav:true,
-        autoWidth:true,
-        singleItem:true
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     })
 });
-
-// $(document).ready(function(){
-//   // Add smooth scrolling to all links
-//   $("#nav-link").on('click', function(event) {
-
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
-
-//       // Store hash
-//       var hash = this.hash;
-
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800, function(){
-
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       });
-//     } // End if
-//   });
-// });
