f4f2b2a01fceccf1e1392e799c4cc18c48549e3d
Wildan Pratama Putra <57258589+wildanptra@users.noreply.github.com>
2021-12-11 21:46:46 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,26 +27,26 @@ $(document).ready(function(){
             }
         }
     })
-    
-    $('.nav-item a').click(function(e) {
-  // Add smooth scrolling to all links
+      // Add smooth scrolling to all links
+      $(".nav-item a").on('click', function(event) {
 
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-  // Prevent default anchor click behavior
-  event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
 
-  // Store hash
-  var hash = this.hash;
+          // Store hash
+          var hash = this.hash;
 
-  // Using jQuery's animate() method to add smooth page scroll
-  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top
-  }, 800, function(){
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
 
-    // Add hash (#) to URL when done scrolling (default click behavior)
-    window.location.hash = hash;
-  });
-});
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
 });
