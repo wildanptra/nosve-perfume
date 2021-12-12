b15ff20b43a652e7933edc0b92c9760c37d272d2
wildanptra <wildanptra03@gmail.com>
2021-12-12 11:44:43 +0700

    update feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,21 +10,69 @@ window.onscroll = () =>  {
     }
 };
 
+window.addEventListener('scroll', () => {
+  // About
+  let about = document.querySelector('#about-page');
+  let aboutPositionTop = about.getBoundingClientRect().top;
+
+  // Product
+  let product = document.querySelector('#product-page');
+  let productPositionTop = product.getBoundingClientRect().top;
+
+  // Gallery
+  let gallery = document.querySelector('#gallery-page');
+  let galleryPositionTop = gallery.getBoundingClientRect().top;
+
+  let innerScreen = window.innerHeight / 1;
+
+  if (aboutPositionTop < innerScreen) {
+    about.classList.add('page-active');
+  } else {
+    about.classList.remove('page-active');
+  }
+
+  if (productPositionTop < innerScreen) {
+    product.classList.add('page-active')
+  } else {
+    product.classList.remove('page-active');
+  }
+
+  if (galleryPositionTop < innerScreen) {
+    gallery.classList.add('page-active')
+  } else {
+    gallery.classList.remove('page-active');
+  }
+
+})
+
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:false,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    })
+  $('.owl-carousel').owlCarousel({
+      loop:false,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  })
+
+  $(".nav-item a").on('click', function(event) {
+    if (this.hash !== "") {          
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 10, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
 });
